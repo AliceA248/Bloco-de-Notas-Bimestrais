a0712c76a98322867438d94d2987ae147d210dd1
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2024-01-02 15:28:59 -0300

    acerto no app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { TodoWrapper } from './Components/TodoWarapper';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <h1>Bloco de Notas Bimestrais</h1>
+      <TodoWrapper />
     </div>
   );
 }
